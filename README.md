a8ed8dbc416025296517229f34e389940f98c3e5
Josteda99 <josteda99@gmail.com>
2026-03-13 12:52:10 -0500

    implement cancel, sub, available funds, fund transaction

--- README.md
+++ README.md
@@ -3,8 +3,8 @@ Requirements:
 * table with avaiable funds✅
 * option to subscribe to a fund if min mont ✅ 
 * sleect notification method (email, sms) ✅
-* cancel participation and check new balance
-* watch a list or table transaction history (subs and cancels)
+* cancel participation and check new balance ✅
+* watch a list or table transaction history (subs and cancels)✅
 * show proper errors if no enoght funds
 * simulate fund movements
 * save in fake DB (localStorage)
--- src/app/app.ts
+++ src/app/app.ts
@@ -15,16 +15,29 @@ interface User {
   subscribedFunds: Fund[];
 }
 
+export interface FundTransaction {
+  id: number;
+  fundName: string;
+  amount: number;
+  transactionDate: Date;
+  type: FundTransactionType;
+}
+
+export enum FundTransactionType {
+  SUBSCRIPTION = 'SUBSCRIPTION',
+  CANCELLATION = 'CANCELLATION',
+}
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('fund-management');
 
-  public initialBalance = signal(50_000);
+  public initialBalance = signal(500_000);
   public user = signal<User>({ name: 'John Doe', email: '', phone: '', subscribedFunds: [] });
 
   public selectedFund = signal<Fund | null>(null);
@@ -35,8 +48,10 @@ export class App {
     { id: 2, name: 'FPV_BTG_PACTUAL_ECOPETROL', minAmount: 125_000, category: 'FPV' },
     { id: 3, name: 'DEUDAPRIVADA', minAmount: 50_000, category: 'FIC' },
     { id: 4, name: 'FDO-ACCIONES', minAmount: 250_000, category: 'FIC' },
-    { id: 4, name: 'FDO-FPV_BTG_PACTUAL_DINAMICA', minAmount: 100_000, category: 'FPV' },
+    { id: 5, name: 'FDO-FPV_BTG_PACTUAL_DINAMICA', minAmount: 100_000, category: 'FPV' },
   ]);
+
+  public fundTransactions = signal<FundTransaction[]>([]);
 
   public subscribeToFund() {
     const fund = this.selectedFund();
@@ -54,6 +69,7 @@ export class App {
     this.user.update((u) => ({ ...u, subscribedFunds: [...u.subscribedFunds, fund] }));
     this.initialBalance.update((balance) => balance - fund.minAmount);
     alert(`Subscribed to ${fund.name} successfully!`);
+    this.addTransactionEntry(fund, FundTransactionType.SUBSCRIPTION);
   }
 
   public selectFund(fund: Fund) {
@@ -62,5 +78,34 @@ export class App {
 
   public changeNotificationPreference(option: string) {
     this.notificationPreference.set(option);
+  }
+
+  public checkSubscription(fundId: number) {
+    const exist = this.user().subscribedFunds.find((fund) => fund.id === fundId);
+
+    return exist ? 'Cancel' : 'Subscribe';
+  }
+
+  public cancelFund() {
+    const fund = this.selectedFund();
+    if (!fund) return;
+    this.user.update((u) => ({
+      ...u,
+      subscribedFunds: [...u.subscribedFunds].filter((f) => f.id !== fund.id),
+    }));
+
+    this.addTransactionEntry(fund, FundTransactionType.CANCELLATION);
+  }
+
+  public addTransactionEntry(fund: Fund, type: FundTransactionType) {
+    let id = this.fundTransactions().length ?? 0;
+    const fundTransaction: FundTransaction = {
+      id: id++,
+      fundName: fund.name,
+      amount: 0, // fix
+      transactionDate: new Date(),
+      type: type,
+    };
+    this.fundTransactions.update((ft) => [...ft, fundTransaction]);
   }
 }
